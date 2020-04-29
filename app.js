da07d71e2ce10f181398ca9b7a3e2bcb53937569
Kellen James <Kellenkjames@gmail.com>
2020-04-28 22:19:05 -0700

    js Alert Banner udpates
    completed functionality for JS alert banner

---
+++ app.js
@@ -0,0 +1,22 @@
+// Create the HTML for the banner
+
+const alertBanner = document.getElementById("alert");
+
+alertBanner.innerHTML = 
+
+`<div class="main__alert__banner"> 
+<p>
+<strong>Alert:</strong> You have <strong>6</strong> overdue tasks to 
+complete.
+</p>
+<p class="main__alert__banner__close">x</p>
+</div>`
+
+// Add click event to the alert banner
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("main__alert__banner__close")) {
+        alertBanner.style.display = "none";
+    }
+    
+});
