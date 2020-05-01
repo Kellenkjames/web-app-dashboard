7d08c00dada43f70a10f2abdc3623f75a986c319
Kellen James <Kellenkjames@gmail.com>
2020-04-30 22:39:59 -0700

    new constructor instance created for dailyChart
    new constructor instance created for dailyChart

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ let trafficData = {
             pointRadius: 6,
             pointHitRadius: 2,
             lineTension: 0
-
+            
         }]
     };
     
@@ -58,15 +58,45 @@ let trafficData = {
         legend : {
             display: false
         },
-    
+        
     };
+    
+    let trafficChart = new Chart(trafficCanvas, {
+        type: 'line',
+        data: trafficData,
+        options: trafficOptions
+    });
+    
+    // Daily Bar (Chart)
+    const dailyCanvas = document.getElementById("daily-chart");
+    
+    // data for daily traffic bar chart
+    const dailyData = {
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
+        datasets: [{
+            label: '# of Hits',
+            data: [75, 115, 175, 125, 225, 200, 100],
+            backgroundColor: '#7477BF',
+            borderWidth: 1
+        }]
+    };
+    const dailyOptions = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        },
+        legend : {
+            display: false
+        }
+    }
 
-let trafficChart = new Chart(trafficCanvas, {
-    type: 'line',
-    data: trafficData,
-    options: trafficOptions
-});
+    let dailyChart = new Chart(dailyCanvas, {   
+        type: 'bar',
+        data: dailyData,
+        options: dailyOptions
 
-// Daily Bar (Chart)
-const dailyCanvas = document.getElementById("daily-chart");
+    });
     
