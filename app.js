ad385eebca383973225e795692b869f0885ad38c
Kellen James <Kellenkjames@gmail.com>
2020-05-05 15:50:47 -0700

    validation added for message form at bottom of the page
    validation added for message form at bottom of the page.

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ let trafficData = {
         type: 'line',
         data: trafficData,
         options: trafficOptions
-
+        
     });
     
     // Daily Chart (Bar)
@@ -143,9 +143,23 @@ let trafficData = {
         options: mobileOptions
         
     });
-
+    
     // Messaging Section
     const user = document.getElementById("userField");
     const message = document.getElementById("messageField");
     const send = document.getElementById("send");
+    
+    send.addEventListener('click', () => {
+        // ensure user and message fields are filled out
+        if (user.value === "" && message.value === "") {
+            alert("Please fill out user and message fields before sending");
+        } else if (user.value === "" ) {
+            alert("Please fill out user field before sending");
+        } else if (message.value === "" ) {
+            alert("Please fill out message field before sending");
+        } else {
+            alert(`Message successfully sent to: ${user.value}`);
+        }
+    });
+    
     
